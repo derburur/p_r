eb8859e39ddcbb3e2be85215616efa32d1a583e5
ninesix77 <ninesix77@gmail.com>
2017-01-22 09:52:32 +0900

    commit

---
+++ string/3.py
@@ -0,0 +1,5 @@
+print('hello world'.capitalize())
+print('hello world'.upper())
+print(len('hello world'))
+print('hello world'.__len__())
+print("Hello world".replace('world', 'programming'))
---
+++ string/4.py
@@ -0,0 +1,5 @@
+print("egoing\'s \"tutorial\"")
+print("\\")
+print("Hello\nworld")
+print("Hello\t\tworld")
+print("\a") #경고음
